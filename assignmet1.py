c668479900de81eeb3b73bd1ef326ef07c1059c4
Charan Sai Penugoti <108610281+CharanSaiPenugoti@users.noreply.github.com>
2023-03-02 20:36:04 +0530

    Add files via upload

---
+++ assignmet1.py
@@ -0,0 +1,8 @@
+print("Enter the Principle value: ")
+p=int(input())
+print("Enter the Time value: ")
+t=int(input())
+print("Enter the Rate value: ")
+r=int(input())
+s=(p*t*r)/100
+print("The simple interest value is "+str(s))
